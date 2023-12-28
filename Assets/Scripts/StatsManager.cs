d38521df8133bb3dd7aaf35eb60a84f597090a48
Enrico Pellini <142680134+epellini@users.noreply.github.com>
2023-12-28 15:14:48 -0500

    Fix StatsManager

--- Assets/Scripts/StatsManager.cs
+++ Assets/Scripts/StatsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,8 @@ using UnityEngine.Events;
 
 public class StatsManager : MonoBehaviour
 {
-    private float _hungerDecreaseRatePerHour = 3000f;
-    private float _thirstDecreaseRatePerHour = 3400f;
+    private float _hungerDecreaseRatePerHour = 4600f;
+    private float _thirstDecreaseRatePerHour = 1800f;
     private const string HungerKey = "Hunger";
     private const string ThirstKey = "Thirst";
 
@@ -18,14 +19,37 @@ public class StatsManager : MonoBehaviour
     [Header("Thirst")]
     private float _maxThirst = 100f;
     private float _currentThirst;
-    public float ThirstPercent => _currentThirst / _maxThirst;
+    public float ThirstPercent => _currentThirst / _maxHunger;
 
     public static UnityAction OnPlayerDeath;
 
-    private float _lastUpdateTime;
+    private DateTime _lastUpdateTime;
+
     private void Start()
     {
-        _lastUpdateTime = Time.realtimeSinceStartup; // Initialize _lastUpdateTime
+        // Initialize _lastUpdateTime using PlayerPrefs or current time
+        string lastPlayTimeStr = PlayerPrefs.GetString("LastPlayTime");
+        if (!string.IsNullOrEmpty(lastPlayTimeStr))
+        {
+            long temp;
+            if (long.TryParse(lastPlayTimeStr, out temp))
+            {
+                _lastUpdateTime = DateTime.FromBinary(temp);
+            }
+            else
+            {
+                // Set a default value for _lastUpdateTime if parsing fails
+                _lastUpdateTime = DateTime.Now;
+                PlayerPrefs.SetString("LastPlayTime", _lastUpdateTime.ToBinary().ToString());
+                PlayerPrefs.Save();
+            }
+        }
+        else
+        {
+            _lastUpdateTime = DateTime.Now;
+            PlayerPrefs.SetString("LastPlayTime", _lastUpdateTime.ToBinary().ToString());
+            PlayerPrefs.Save();
+        }
 
         // Check if PlayerPrefs have been set for hunger and thirst, and if not, use default values.
         if (!PlayerPrefs.HasKey(HungerKey) || !PlayerPrefs.HasKey(ThirstKey))
@@ -35,44 +59,52 @@ public class StatsManager : MonoBehaviour
         }
         else
         {
-            float loadedHunger, loadedThirst;
-            LoadStats(out loadedHunger, out loadedThirst);
-            
-            // Retrieve the exit time when the game was last quit
-            float lastPlayTime = PlayerPrefs.GetFloat("LastPlayTime", Time.realtimeSinceStartup);
-
-            // Calculate time passed since the game was last played.
-            float currentTime = Time.realtimeSinceStartup;
-            //float lastPlayTime = PlayerPrefs.GetFloat("LastPlayTime", currentTime);
-
-            // Ensure that timePassed is always positive.
-            float timePassed = lastPlayTime > currentTime ? 0 : currentTime - lastPlayTime;
-
-            // Update the stats based on the time passed.
-            _currentHunger = loadedHunger - (_hungerDecreaseRatePerHour / 3600) * timePassed;
-            _currentThirst = loadedThirst - (_thirstDecreaseRatePerHour / 3600) * timePassed;
-
-            // Ensure that stats don't go below zero or exceed their maximum values.
-            _currentHunger = Mathf.Clamp(_currentHunger, 0, _maxHunger);
-            _currentThirst = Mathf.Clamp(_currentThirst, 0, _maxThirst);
-
-            Debug.Log($"Loaded Hunger: {loadedHunger}, Loaded Thirst: {loadedThirst}");
-            Debug.Log($"Current Hunger: {_currentHunger}, Current Thirst: {_currentThirst}");
-            Debug.Log($"Time Passed: {timePassed}");
+            // Load the saved hunger and thirst values from PlayerPrefs
+            _currentHunger = PlayerPrefs.GetFloat(HungerKey, _maxHunger);
+            _currentThirst = PlayerPrefs.GetFloat(ThirstKey, _maxThirst);
         }
-    }
 
+
+        // Load player stats (hunger and thirst) from PlayerPrefs
+        LoadStats(out _currentHunger, out _currentThirst);
+
+        // Calculate time passed using TimeSpan
+        TimeSpan timePassed = DateTime.Now - _lastUpdateTime;
+
+        // // Decrease hunger and thirst based on the time passed and decrease rates per hour.
+        // float hungerDecrease = (_hungerDecreaseRatePerHour / 3600) * (float)timePassed.TotalSeconds;
+        // float thirstDecrease = (_thirstDecreaseRatePerHour / 3600) * (float)timePassed.TotalSeconds;
+
+        // _currentHunger -= hungerDecrease;
+        // _currentThirst -= thirstDecrease;
+
+        // Ensure that stats don't go below zero or exceed their maximum values.
+        _currentHunger = Mathf.Clamp(_currentHunger, 0, _maxHunger);
+        _currentThirst = Mathf.Clamp(_currentThirst, 0, _maxThirst);
+
+        // Store the current time as the last update time
+        _lastUpdateTime = DateTime.Now;
+
+        PlayerPrefs.SetString("LastPlayTime", _lastUpdateTime.ToBinary().ToString());
+        PlayerPrefs.Save();
+
+        Debug.Log($"Current Hunger: {_currentHunger}, Current Thirst: {_currentThirst}");
+        Debug.Log($"Time Passed: {timePassed}");
+    }
 
     private void Update()
     {
-        // Calculate time passed since the last frame.
-        float currentTime = Time.realtimeSinceStartup;
-        float timePassed = currentTime - _lastUpdateTime;
+        // Calculate time passed since the last frame using DateTime.
+        DateTime currentTime = DateTime.Now;
+        TimeSpan timePassed = currentTime - _lastUpdateTime;
         _lastUpdateTime = currentTime;
 
+        // Convert time passed to hours (assuming your decrease rates are per hour).
+        float hoursPassed = (float)timePassed.TotalHours;
+
         // Decrease hunger and thirst based on the time passed and decrease rates per hour.
-        float hungerDecrease = _hungerDecreaseRatePerHour / 3600 * timePassed;
-        float thirstDecrease = _thirstDecreaseRatePerHour / 3600 * timePassed;
+        float hungerDecrease = _hungerDecreaseRatePerHour * hoursPassed;
+        float thirstDecrease = _thirstDecreaseRatePerHour * hoursPassed;
 
         if (_currentHunger > 0)
         {
@@ -94,12 +126,14 @@ public class StatsManager : MonoBehaviour
 
         if (_currentHunger <= 0 && _currentThirst <= 0)
         {
-            //OnPlayerDeath?.Invoke();
-            //Debug.Log("You died");
+            // Handle player death or any other relevant logic.
+            // OnPlayerDeath?.Invoke();
+            // Debug.Log("You died");
             _currentHunger = 0;
             _currentThirst = 0;
         }
     }
+
 
     public void IncreaseHunger(float hungerAmount)
     {
@@ -121,8 +155,11 @@ public class StatsManager : MonoBehaviour
 
     private void OnApplicationQuit()
     {
-        //PlayerPrefs.SetFloat("LastPlayTime", _lastUpdateTime); // Save the initial time when quitting
-        PlayerPrefs.SetFloat("LastPlayTime", Time.realtimeSinceStartup); // Save the initial time when quitting
+        // Save the current system time as a string in the player prefs class
+        long currentSystemTime = System.DateTime.Now.ToBinary();
+        PlayerPrefs.SetString("sysString", currentSystemTime.ToString());
+
+        print("Saving this date to prefs: " + System.DateTime.Now);
         SaveStats(_currentHunger, _currentThirst);
     }
 
@@ -135,20 +172,37 @@ public class StatsManager : MonoBehaviour
 
     private void LoadStats(out float currentHunger, out float currentThirst)
     {
-        float lastPlayTime = PlayerPrefs.GetFloat("LastPlayTime", Time.realtimeSinceStartup);
-        float currentTime = Time.realtimeSinceStartup;
-        float timePassed =  currentTime - lastPlayTime;
-
         currentHunger = PlayerPrefs.GetFloat(HungerKey, 100f); // Default value of 100
         currentThirst = PlayerPrefs.GetFloat(ThirstKey, 100f); // Default value of 100
+        
+        if (PlayerPrefs.HasKey("sysString"))
+        {
+            // Get the old system time from player prefs as a string
+            long temp = Convert.ToInt64(PlayerPrefs.GetString("sysString"));
 
-        // Decrease the stats based on the time passed.
-        currentHunger += _hungerDecreaseRatePerHour / 3600 * timePassed;
-        currentThirst += _thirstDecreaseRatePerHour / 3600 * timePassed;
+            // Convert the old time from binary to a DateTime variable
+            DateTime oldSystemTime = DateTime.FromBinary(temp);
 
-        // Ensure that stats don't go below zero or exceed their maximum values.
-        currentHunger = Mathf.Clamp(currentHunger, 0, _maxHunger);
-        currentThirst = Mathf.Clamp(currentThirst, 0, _maxThirst);
+            // Get the current system time
+            DateTime currentSystemTime = System.DateTime.Now;
+
+            // Calculate the time passed as a TimeSpan
+            TimeSpan timePassed = currentSystemTime - oldSystemTime;
+
+            // Convert timePassed to hours
+            float timePassedInHours = (float)timePassed.TotalHours;
+
+            currentHunger = PlayerPrefs.GetFloat(HungerKey, 100f); // Default value of 100
+            currentThirst = PlayerPrefs.GetFloat(ThirstKey, 100f); // Default value of 100
+
+            // Decrease the stats based on the time passed.
+            currentHunger -= _hungerDecreaseRatePerHour * timePassedInHours;
+            currentThirst -= _thirstDecreaseRatePerHour * timePassedInHours;
+
+            // Ensure that stats don't go below zero or exceed their maximum values.
+            currentHunger = Mathf.Clamp(currentHunger, 0, _maxHunger);
+            currentThirst = Mathf.Clamp(currentThirst, 0, _maxThirst);
+        }
     }
 }
 
